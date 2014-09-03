243e58df6389e3dad1c9ff6925303157b6d9a055
Dan Lipert <dan@hyperlayer.io>
2014-09-03 15:12:01 -0700

    moving color check to haaring stage

--- hyperlayer/haar.py
+++ hyperlayer/haar.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy
 from math import sin, cos, radians
+from util.image import isHumanColor
 
 def rotate_image(image, angle):
     if angle == 0: return image
@@ -23,7 +24,7 @@ def ints_only(box):
 def correct_cropping_region(box, region):
     return [box[0]+region['x'], box[1]+region['y'], box[2], box[3]]
 
-def detectFaces(image, detectors):
+def detectFaces(image, colorImage, detectors):
     """
     Returns list of bounding boxes (x, y, w, h) containing human faces.
     
@@ -52,14 +53,80 @@ def detectFaces(image, detectors):
             boxes = list(boxes)
             if len(boxes):
                 for box in boxes:
-                    print box
                     box = rotate_point(box, image, -angle)
-                    print box
+
                     box = correct_cropping_region(box, region)
                     box = ints_only(box)
-                    all_boxes.append(box)
-                    print '%s faces found in frame' % len(all_boxes)
+                    croppedFace = colorImage[box[1]:box[1]+box[3], box[0]:box[0]+box[2]]
+                    if isHumanColor(croppedFace):
+                        all_boxes.append(box)
+                        print '%s faces found in frame' % len(all_boxes)
     return all_boxes
+
+def boxesOverlap(boxA, boxB):
+    '''
+    determines if two boxes overlap
+    http://stackoverflow.com/questions/306316/determine-if-two-rectangles-overlap-each-other
+    '''
+    
+    ax1 = boxA[0]
+    ax2 = boxA[0]+boxA[2]
+    ay1 = boxA[1]
+    ay2 = boxA[1]+boxA[3]
+    
+    bx1 = boxB[0]
+    bx2 = boxB[0]+boxB[2]
+    by1 = boxB[1]
+    by2 = boxB[1]+boxB[3]
+    
+    if ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1:
+        return True
+    else:
+        return False
+
+def combinedBox(boxA, boxB):
+    #first lets assign the top box as box A, regardless of how we received them
+    if boxA[0] > boxB[0]:
+        #swap
+        print 'swapped'
+        tempBox = boxA
+        boxA = boxB
+        boxB = tempBox
+    
+    ax1 = boxA[0]
+    ax2 = boxA[0]+boxA[2]
+    ay1 = boxA[1]
+    ay2 = boxA[1]+boxA[3]
+    
+    bx1 = boxB[0]
+    bx2 = boxB[0]+boxB[2]
+    by1 = boxB[1]
+    by2 = boxB[1]+boxB[3]
+    
+
+        
+    
+    if ax1 > bx1:
+        cx1 = bx1
+    else:
+        cx1 = ax1
+    
+    if ay1 < by1:
+        cy1 = ay1
+    else:
+        cy1 = by1
+    
+    if ax2 > bx2:
+        cx2 = ax2
+    else:
+        cx2 = bx2
+    
+    if ay2 > by2:
+        cy2 = by2
+    else:
+        cy2 = ay2
+    
+    return [cx1, cy1, cx2 - cx1, cy2 - cy1]
 
 def muxBoxes(boxes, minSize=(0,0)):
     """
@@ -68,5 +135,18 @@ def muxBoxes(boxes, minSize=(0,0)):
     boxes -- list of boxes to mux
     minSize -- boxes below this size (both width and height) will be discarded
     """
-    
     return boxes
+    '''
+    muxedBoxes = []
+    
+    for eachBox in boxes:
+        boxOverlapped = False
+        for eachOtherBox in boxes:
+            if boxesOverlap(eachBox, eachOtherBox) and eachBox != eachOtherBox:
+                boxOverlapped = True
+                muxedBoxes.append(combinedBox(eachBox, eachOtherBox))
+        if boxOverlapped == False:
+            muxedboxes.append(eachBox)
+
+    return boxes
+    '''
